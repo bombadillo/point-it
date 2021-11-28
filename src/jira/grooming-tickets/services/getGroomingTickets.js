957c0fa62a31324ec71384b5f2c0eff917749929
bombadillo <mumfordnsons@gmail.com>
2021-11-28 19:42:15 +0000

    Use default url for netlify functions

--- src/jira/grooming-tickets/services/getGroomingTickets.js
+++ src/jira/grooming-tickets/services/getGroomingTickets.js
@@ -1,7 +1,5 @@
 export default async () => {
-  const response = await fetch(
-    'http://localhost:63349/.netlify/functions/jira-grooming-tickets'
-  )
+  const response = await fetch('/.netlify/functions/jira-grooming-tickets')
 
   if (response.ok) {
     return await response.json()

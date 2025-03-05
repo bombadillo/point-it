ef0faf3f767979da696ac94eb31f505df1e2b954
bombadillo <mumfordnsons@gmail.com>
2025-03-05 11:42:08 +0000

    Remove slate text

--- src/game/create/CreateGame.vue
+++ src/game/create/CreateGame.vue
@@ -16,7 +16,7 @@
                                 name="username"
                                 type="text"
                                 required
-                                class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                 placeholder="Your name"
                             />
                         </div>
